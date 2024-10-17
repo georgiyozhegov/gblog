52998e6b19ac8e81ba555e118387c385368d59dd
Georgiy Ozhegov <georgiyozhegov.rs@gmail.com>
2024-10-17 13:06:04 +0400

    First working version

---
+++ generate.py
@@ -0,0 +1,137 @@
+from datetime import datetime
+import markdown
+import sys
+import os
+
+# (feed, ps)
+PAGE_TEMPLATE = """
+<!doctype html>
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <h1><span class="g">G</span>Блог</h1>
+    <ul class="feed">
+%s
+    </ul>
+    <div class="ps">
+%s
+    </div>
+  </body>
+</html>
+""".strip()
+
+# (title, meta, link)
+ARTICLE_PREVIEW_TEMPLATE = """
+<div class="article-preview">
+  <h2 class="article-preview_title">%s</h2>
+  <div class="article-preview_inner">
+    <div class="article-preview_meta">
+%s
+    </div>
+  <button class="article-preview_link" onclick="window.location.href='%s'">Читать</button>
+  </div>
+</div>
+""".strip()
+
+# (text)
+META_TEMPLATE = """
+<p class="article-preview_meta_item">%s</p>
+""".strip()
+
+# (text)
+PS_TEMPLATE = """
+<p class="ps_inner">%s</p>
+""".strip()
+
+# (text, ps)
+ARTICLE_TEMPLATE = """
+<!doctype html>
+<html>
+  <head>
+    <link rel="stylesheet" href="../style.css">
+  </head>
+  <body>
+    <h1><span class="g">G</span>Блог</h1>
+    <div class="article">
+      <div class="article_navigation">
+        <button class="article_navigation_back" onclick="window.location.href='../index.html'">Назад</button>
+      </div>
+      <div class="article_content">
+%s
+      </div>
+    </div>
+  </body>
+</html>
+""".strip()
+
+class Meta:
+    def __init__(self, read_time):
+        self.date = datetime.today().strftime("%d.%m.%Y")
+        self.read_time = read_time
+
+    def __repr__(self):
+        return META_TEMPLATE % ("Дата: " + self.date) + "\n" + \
+               META_TEMPLATE % ("Время чтения: " + str(self.read_time) + "м")
+
+class Article:
+    def __init__(self, title, meta, link):
+        self.title = title
+        self.meta = meta
+        self.link = link
+
+    def __repr__(self):
+        return ARTICLE_PREVIEW_TEMPLATE % (self.title, self.meta, self.link)
+
+def main():
+    try:
+        feed_directory = sys.argv[1]
+    except:
+        print("error: specify feed directory")
+        exit(1)
+    try:
+        output_directory = sys.argv[2]
+    except:
+        print("error: specify output directory")
+        exit(1)
+    
+    if os.path.exists(output_directory):
+        print(f"info: removing old output directory")
+        os.system("rm -rf " + output_directory)
+
+    print(f"info: creating new output directory")
+    os.mkdir(output_directory)
+    os.mkdir(output_directory + "/feed")
+
+    os.system(f"cp style.css {output_directory}/style.css")
+
+    feed_list = os.listdir(feed_directory)
+    feed_list = [file_path for file_path in feed_list if file_path.endswith(".md")]
+    feed_files = [open(feed_directory + "/" + file_path).read() for file_path in feed_list]
+
+    main_page_html = ""
+    for file_name, file_content in zip(feed_list, feed_files):
+        output_file_path = "feed" + "/" + file_name.rsplit(".", 1)[0] + ".html"
+        print(f"info: generating html for '{file_name}' at '{output_directory + '/' + output_file_path}'")
+
+        title, content = file_content.split("\n\n", 1)
+        title = title[1:].strip()
+        content = content.strip()
+
+        with open(output_directory + "/" + output_file_path, "w") as file:
+            content_html = markdown.markdown(content, extensions=["markdown.extensions.fenced_code", "markdown.extensions.codehilite"])
+            file.write(ARTICLE_TEMPLATE % (content_html))
+        
+        read_time = int((len(file_content) / 5.28) / 250)
+        article = Article(title, Meta(read_time), output_file_path)
+        main_page_html += str(article) + "\n"
+
+    main_page_html = PAGE_TEMPLATE % (main_page_html, "")
+
+    print(f"info: generating main page html at '{output_directory + '/index.html'}'")
+    with open(output_directory + "/" + "index.html", "w") as main_page_file:
+        main_page_file.write(main_page_html)
+
+if __name__ == "__main__":
+    main()
